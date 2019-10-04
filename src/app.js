ae4545358dbb01ffe30887f120fc5aac5b6867d2
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-10-04 11:39:20 -0300

    first commit, with configs and begin user module complete

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.server.use(cors());
+    this.server.use(express.json());
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {}
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ src/database/migrations/20191004135107-create-users.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize
+      .query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+      .then(() => {
+        return queryInterface.createTable('users', {
+          id: {
+            allowNull: false,
+            primaryKey: true,
+            type: Sequelize.DataTypes.UUID,
+            defaultValue: Sequelize.literal('uuid_generate_v4()'),
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+          },
+          password_hash: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+          updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+        });
+      });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
